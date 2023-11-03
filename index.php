a65eb67563aa96dbb7f20c329d852baf566169da
vikAyz9rY4Y7gT5 <KHALIDLOGI@GMAIL.COM>
2023-11-03 22:28:55 +0100

    jj

--- index.php
+++ index.php
@@ -1,2 +1,40 @@
-<?php
-// Silence is golden.
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <h1>My Portfolio</h1>
+    </header>
+
+    <section class="portfolio">
+        <div class="portfolio-item">
+            <img src="project1.jpg" alt="Project 1">
+            <h2>Project 1</h2>
+            <p>Description of Project 1.</p>
+            <a href="project1.html">View Project</a>
+        </div>
+
+        <div class="portfolio-item">
+            <img src="project2.jpg" alt="Project 2">
+            <h2>Project 2</h2>
+            <p>Description of Project 2.</p>
+            <a href="project2.html">View Project</a>
+        </div>
+
+        <!-- Add more portfolio items as needed -->
+
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Your Name</p>
+    </footer>
+</body>
+
+</html>
